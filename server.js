15c19748e5ee55c7c1de512946fc3cfdf14ae603
Tom Gallacher <tomgallacher23@gmail.com>
2012-04-05 19:00:01 +0100

    Fixing various bugs and adding an mdns advertisement

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ require('console-trace');
 // console.traceAlways = true;
 
 var socketio = require('socket.io')
-  , io = socketio.listen(8282)
+  , mdns = require('mdns')
+  , app = require('http').createServer()
+  , io = socketio.listen(app)
   , managers = require('./lib/discovery').managers
   , context  = require('zmq')
   , OnlineHelper = require('./lib/onlineHelper')
@@ -10,34 +12,49 @@ var socketio = require('socket.io')
   , connectedTo = null
   , client
   , Workunit = require('./lib/workunit')
+  , workunit
+  , zmqSocket
+  , zmqOnline = false
+  , socketioOnline = false
+  , offlineMessageQueue = []
+  , packageJSON = require('./package.json')
+  , appVersion = 'v' + packageJSON.version.split('.').slice(0, -1).join('-')
   ;
 
-io.set("log level", 1);
+io.set("log level", 0);
 
 /**
  *  Connect to parent Manager, with port.
  */
-// receiver.connect('tcp://localhost:60000');
-function startSocketIO(workunit, zmqSocket) {
+
+function startSocketIO(workunit) {
+
   io.sockets.on('connection', function (socketio) {
+    socketioOnline = true;
     socketio.on('message', function(message) {
-      console.log(message);
+      var socket = {};
+      if (!zmqOnline) {
+        socket.send = function(msg) {
+          offlineMessageQueue.push(msg);
+        };
+      } else {
+        socket = zmqSocket;
+      }
       switch (message.action) {
         case 'request':
           socketio.json.send({
               "action": "workunit"
-            , "id": workunit.getId()
+            , "workunitId": workunit.getId()
             , "data": workunit.get()
           });
           break;
         case 'getPayload':
           workunit.retrievePayload(function(payload) {
-            console.log(payload);
             socketio.json.send({'action': 'payload', 'data': payload});
           });
           break;
         case 'completed':
-          zmqSocket.send(JSON.stringify(message));
+          socket.send(JSON.stringify(message));
           // socketio.json.send({"action": "workunit", "id":"1337", "data": workunit.get()}); // new task
           break;
         default:
@@ -85,6 +102,10 @@ function subscribeToManager() {
     // if it has, wait until it comes up again, by queing and sending old workunits - if timeout is reached then fetch new manager
     // else if it hasn't start looking for new unit);
     client.on('offline', function() {
+      if (zmqOnline) {
+        zmqSocket.close();
+      }
+      zmqOnline = false;
       startProcess();
       console.log('client down.');
     });
@@ -100,25 +121,33 @@ function subscribeToManager() {
 
 function zmqConnect(service) {
   var uri = 'tcp://' + service.addresses[0] + ':' + service.txtRecord.zmqPort
-    , zmqSocket = context.socket('req')
     ;
+  zmqSocket = context.socket('req');
   zmqSocket.identity = 'distributor' + process.pid;
   zmqSocket.connect(uri);
   console.log('connected to -> ' + uri);
+  zmqOnline = true;
   connectedTo = uri;
   //TODO: check for existing results if found send.
+  if (offlineMessageQueue.length > 0) {
+    offlineMessageQueue.forEach(function(value) {
+      zmqSocket.send(value);
+    });
+  }
   zmqSocket.send('{"action":"requestWorkunit"}');
   zmqSocket.on('message', function(buf) {
     var obj = JSON.parse(buf.toString());
     switch(obj.action) {
       case 'workunit':
-        var workunit = Workunit.createWorkunit(obj.id, obj.workunit, obj.payloads);
-        startSocketIO(workunit, zmqSocket);
+        workunit = Workunit.createWorkunit(obj.id, obj.workunit, obj.payloads);
+        if (!socketioOnline) startSocketIO(workunit);
         break;
       case 'saved':
+        workunit.completePayload(obj.data.id);
         // log that it was saved and clear queue items that saved.
         break;
       case 'notSaved':
+        console.error(new Error('Could not save.'));
         // add to a queue to be sent with next iteration
         break;
       default:
@@ -126,5 +155,18 @@ function zmqConnect(service) {
     }
   });
 }
+app.listen(function() {
+  // need to publish somehow so others can connect.
+  startDiscovery(app.address().port);
+  startProcess();
+});
 
-startProcess();
+/**
+ *  Starts the Bonjour / zeroconf service for advertising up services
+ *  and logs them
+ */
+function startDiscovery(port) {
+  console.log('Running disio-distributor' + '@' + appVersion + ' on ' + '0.0.0.0:' + port);
+  var ad = mdns.createAdvertisement(mdns.udp('disio-distribu', appVersion), port, {'txtRecord': { name: 'dis.io distributor' }});
+  ad.start();
+}
